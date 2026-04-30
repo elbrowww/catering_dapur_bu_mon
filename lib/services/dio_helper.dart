09560e85de0420215b7a7ccbf96e3fb8ec5c5b9f
ChennT16 <zedred63@gmail.com>
2026-04-30 09:43:49 +0700

    .

--- lib/services/dio_helper.dart
+++ lib/services/dio_helper.dart
@@ -5,7 +5,7 @@ class DioHelper {
   static late Dio _dio;
 
   // Ganti IP ini sesuai IP server/XAMPP Anda
-  static const String baseUrl = 'http://172.16.103.197/dapur_bu_mon/api/';
+  static const String baseUrl = 'http://172.16.202.86/dapur_bu_mon/api/';
 
   static void init() {
     _dio = Dio(
